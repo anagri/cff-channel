fa2a3d33ebf70e62361e7f376f522ec79c42b798
anagri <127566+anagri@users.noreply.github.com>
2024-11-30 14:03:28 +0530

    [Amir] having config release, fixing copy path on windows

--- crates/cffi-channel/Makefile.win.mk
+++ crates/cffi-channel/Makefile.win.mk
@@ -6,14 +6,14 @@ OUTPUT_DIR = libs
 
 build:
 	@pwsh -Command "cmake -S csrc -B $(BUILD_DIR)"
-	@pwsh -Command "cmake --build $(BUILD_DIR)"
+	@pwsh -Command "cmake --build $(BUILD_DIR) --config Release"
 
 clean:
 	@pwsh -Command "if (Test-Path $(BUILD_DIR)) { Remove-Item -Recurse -Force $(BUILD_DIR) }"
 
 copy:
 	@pwsh -Command "if (-not (Test-Path $(OUTPUT_DIR))) { New-Item -ItemType Directory -Force $(OUTPUT_DIR) }"
-	@pwsh -Command "Copy-Item $(BUILD_DIR)/$(LIBNAME) -Destination $(OUTPUT_DIR)/$(LIBNAME) -Force"
+	@pwsh -Command "Copy-Item $(BUILD_DIR)/Release/$(LIBNAME) -Destination $(OUTPUT_DIR)/$(LIBNAME) -Force"
 
 ci.build: clean build copy
 
